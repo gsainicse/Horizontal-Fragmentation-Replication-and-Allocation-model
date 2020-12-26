893a70487c388f8bd99cde15f869ad770d18419d
Gurdeep Saini <46388485+gsainicse@users.noreply.github.com>
2020-12-26 17:31:55 +0530

    Update and rename fragmentation.py to FragmentationAndAllocationWithoutReplication.py

--- FragmentationAndAllocationWithoutReplication.py
+++ FragmentationAndAllocationWithoutReplication.py
@@ -7,26 +7,20 @@ samphat = psycopg2.connect(host="10.100.53.25",   port=2345, database="gurdeep",
 kushal = psycopg2.connect(host="10.100.54.83",port=2345, database="gurdeep", user="postgres", password="it701");
 niranjan= psycopg2.connect(host="10.100.53.121",port=5432, database="gurdeep", user="postgres", password="it701");
 
-
 print("All servers are connected")
 
 cur_samphat = samphat.cursor()
 cur_kushal = kushal.cursor()
 cur_niranjan = niranjan.cursor()
 
-
-
 cur_samphat.execute("DROP TABLE employeeone;");
 cur_kushal.execute("DROP TABLE  employeetwo;");
 cur_niranjan.execute("DROP TABLE employeethree;");
-
-
 
 #create tables 
 cur_samphat.execute("CREATE TABLE employeeone (birthdate varchar(50),deptid integer,jobid varchar(50),location varchar(50),name varchar(50) ,salary integer );");
 cur_kushal.execute("CREATE TABLE employeetwo (birthdate varchar(50),deptid integer,jobid varchar(50),location varchar(50),name varchar(50) ,salary integer  );");
 cur_niranjan.execute("CREATE TABLE employeethree (birthdate varchar(50),deptid integer,jobid varchar(50),location varchar(50),name varchar(50) ,salary integer  );");
-
 
 #write to servers
 length=1
@@ -35,10 +29,6 @@ with open('employee2.csv','r') as f:
    
     for row in reader:
         length=length+1
-
-
-
-
 
 with open('employee2.csv','r') as f:
     reader = csv.reader(f) 
@@ -52,7 +42,6 @@ with open('employee2.csv','r') as f:
         e=row[4]
         f=row[5]
         
-
         if int(f) < 1500:
                        
                 cur_samphat.execute("INSERT INTO employeeone VALUES (%s,%s,%s,%s,%s,%s)", (a,b,c,d,e,f))
@@ -73,5 +62,4 @@ cur_niranjan.close()
 samphat.commit()
 kushal.commit()
 niranjan.commit()
-
 
